609ed5d1a1e6476812cda8e77b4a024d8df2e1a8
Strocs <strocsdev@gmail.com>
2024-10-04 18:07:00 -0300

    chore: return to react version

--- src/components/2_proyecto/Team.jsx
+++ src/components/2_proyecto/Team.jsx
@@ -1,16 +1,15 @@
-import { TeamList } from "./TeamList"
-import '../../styles/2_proyecto/Team.css'
+import { TeamList } from "./TeamList";
+import "../../styles/2_proyecto/Team.css";
 
-export const Team = ({name, teamList}) => {
-
+export const Team = ({ name, teamList }) => {
   return (
     <div className={`team__container`}>
-      <h2 className={`team__title`}>
-        {name}
-      </h2>
-      <div className={`team__list`}>
-        {teamList.map(ppl => <TeamList data={ppl} key={ppl.id}/>)}
+      <h2 className={`team__title`}>{name}</h2>
+      <div className={`team__list ${name}`}>
+        {teamList.map((ppl) => (
+          <TeamList data={ppl} key={ppl.id} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/7_gallery/Gallery.jsx
+++ src/components/7_gallery/Gallery.jsx
@@ -1,42 +1,45 @@
-import { useEffect } from "react"
-import { useTransition, animated } from "react-spring"
+import { useEffect } from "react";
+import { useTransition, animated } from "react-spring";
 
-import { Slider } from "./slider"
+import { Slider } from "./Slider";
 
-import "../../styles/7_gallery/Gallery.css"
+import "../../styles/7_gallery/Gallery.css";
 
-export const Gallery = ({toggleGallery, state, data}) => {
+export const Gallery = ({ toggleGallery, state, data }) => {
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.classList.add("scrollLock");
 
-	useEffect(() => {
-		const body = document.querySelector('body')
-		body.classList.add('scrollLock')
+    return () => {
+      body.classList.remove("scrollLock");
+    };
+  }, []);
 
-		return () => {
-			body.classList.remove('scrollLock')
-		}
-	}, [])
-	
-	const transition = useTransition(state, {
-		from: { opacity: 0},
-		enter: { opacity: 1},
-		leave: {opacity: 0},
-		delay: 20,
-	})
+  const transition = useTransition(state, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    delay: 20,
+  });
 
-	return (
-		<>
-			{transition(
-				(style, item) =>
-					item && (
-						<animated.div className="gallery" style={style}>
-							<div className="gallery__container">
-								<span onClick={() => toggleGallery()} className="gallery__close">&#10006;</span>
-								<Slider data={data} />
-
-							</div>
-						</animated.div>
-					)
-			)}
-		</>
-	)
-}
+  return (
+    <>
+      {transition(
+        (style, item) =>
+          item && (
+            <animated.div className="gallery" style={style}>
+              <div className="gallery__container">
+                <span
+                  onClick={() => toggleGallery()}
+                  className="gallery__close"
+                >
+                  &#10006;
+                </span>
+                <Slider data={data} />
+              </div>
+            </animated.div>
+          )
+      )}
+    </>
+  );
+};
