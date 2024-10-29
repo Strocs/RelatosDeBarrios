de684570f95ae0b8d6e9306b8548bdcea6fc3bd3
Strocs <strocsdev@gmail.com>
2024-10-29 16:23:26 -0300

    fix: disabled message on recorridos

--- src/components/4_imaginario/Imaginario.jsx
+++ src/components/4_imaginario/Imaginario.jsx
@@ -5,34 +5,34 @@ import { ARCHIVO_IMG_DATA, ACTIV_IMG_DATA, AEREAS_IMG_DATA } from "../../data/ga
 
 import "../../styles/4_imaginario/Imaginario.css"
 
-export const Imaginario = ({toggleGallery}) => {
-	return (
-		<section className="imaginario">
-			<div className="imaginario__img"></div>
+export const Imaginario = ({ toggleGallery }) => {
+  return (
+    <section className="imaginario">
+      <div className="imaginario__img"></div>
 
-			<div id="Archivo Fotográfico" className="wrappers imaginario__container">
-				<div className="imaginario__title-wrapper">
-					<Title className={"imaginario__title"} title={"Imaginario Urbano"} subTitle={"Imaginario Barrial"} br={true} />
-					<div className="subBars imaginario__subBar"></div>
-				</div>
+      <div id="Archivo Fotográfico" className="wrappers imaginario__container">
+        <div className="imaginario__title-wrapper">
+          <Title className={"imaginario__title"} title={"Imaginario Urbano"} subTitle={"Imaginario Barrial"} br={true} />
+          <div className="subBars imaginario__subBar"></div>
+        </div>
 
-				<div className="imaginario__content">
-					<div className="imaginario__fotos">
-						<Box onClick={toggleGallery} data={ARCHIVO_IMG_DATA} className={"archivo"} title={"Archivo"} subTitle={"Fotográfico"} />
-						<Box onClick={toggleGallery} data={ACTIV_IMG_DATA} className={"actividades"} title={"Fotografías"} subTitle={"Actividades"} />
-						<Box onClick={toggleGallery} data={AEREAS_IMG_DATA} className={"aereas"} title={"Fotografías"} subTitle={"Aéreas"} />
-					</div>
+        <div className="imaginario__content">
+          <div className="imaginario__fotos">
+            <Box onClick={toggleGallery} data={ARCHIVO_IMG_DATA} className={"archivo"} title={"Archivo"} subTitle={"Fotográfico"} />
+            <Box onClick={toggleGallery} data={ACTIV_IMG_DATA} className={"actividades"} title={"Fotografías"} subTitle={"Actividades"} />
+            <Box onClick={toggleGallery} data={AEREAS_IMG_DATA} className={"aereas"} title={"Fotografías"} subTitle={"Aéreas"} />
+          </div>
 
-					<div className="imaginario__3d">
-						<a target="_blank" href="">
-							<Box className={"exterior"} title={"Recorrido 3D"} subTitle={"Exterior"} />
-						</a>
-						<a target="_blank" href="">
-							<Box className={"interior"} title={"Recorrido 3D"} subTitle={"Interior"} />
-						</a>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+          <div className="imaginario__3d">
+            <div>
+              <Box className={"exterior"} title={"Recorrido 3D"} subTitle={"Exterior"} />
+            </div>
+            <div>
+              <Box className={"interior"} title={"Recorrido 3D"} subTitle={"Interior"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
