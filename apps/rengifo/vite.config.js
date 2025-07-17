5b3799bb0515071c65a01841f561d6c0a77d376a
Strocs <strocsdev@gmail.com>
2025-07-17 15:22:09 -0400

    fix(vite): Restore base path in vite.config.js to '/conjuntohabitacionalrengifo'

--- apps/rengifo/vite.config.js
+++ apps/rengifo/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgLoader from '@andylacko/vite-svg-react-loader'
-import { brandConfig } from '@relatos/content'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: brandConfig.paths.rengifo,
+  base: '/conjuntohabitacionalrengifo',
   plugins: [react(), svgLoader()],
 })
