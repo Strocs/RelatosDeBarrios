c5d02d1a031802ba7da8115835735956669bca04
Strocs <strocsdev@gmail.com>
2025-07-17 17:34:58 -0400

    fix(middleware): Refactor middleware logic for improved readability and consistency

--- apps/hub/src/middleware.ts
+++ apps/hub/src/middleware.ts
@@ -1,19 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server';
-
+import { NextRequest, NextResponse } from 'next/server'
 export default function middleware(request: NextRequest) {
-  const host = request.headers.get('host');
-  const url = request.nextUrl.clone();
-
-  if (host?.endsWith('vercel.app')) {
+  if (
+    request.nextUrl.pathname.startsWith('/_next') ||
+    request.nextUrl.pathname.startsWith('relatosdebarrios.cl')
+  ) {
+    return NextResponse.next()
+  }
+  if (request.headers.get('host')?.endsWith('vercel.app')) {
+    const url = request.nextUrl.clone()
     if (url.pathname.startsWith('/villacovico')) {
-      url.hostname = 'relatosdebarrios-covico.vercel.app/villacovico';
-      return NextResponse.rewrite(url);
+      url.hostname = 'relatosdebarrios-covico.vercel.app/villacovico'
+      return NextResponse.rewrite(url)
     }
     if (url.pathname.startsWith('/conjuntohabitacionalrengifo')) {
-      url.hostname = 'relatosdebarrios-rengifo.vercel.app/conjuntohabitacionalrengifo';
-      return NextResponse.rewrite(url);
+      url.hostname =
+        'relatosdebarrios-rengifo.vercel.app/conjuntohabitacionalrengifo'
+      return NextResponse.rewrite(url)
     }
   }
-
-  return NextResponse.next();
+  return NextResponse.next()
 }
