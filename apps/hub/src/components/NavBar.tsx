31ef2ecaa725295389cf8dc7ffef50027922e430
Strocs <strocsdev@gmail.com>
2025-07-17 14:11:27 -0400

    refactor(hub): Centralize hub configuration in @relatos/content

--- apps/hub/src/components/NavBar.tsx
+++ apps/hub/src/components/NavBar.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link'
-import data from '@/config/config.json'
+import { hubConfig } from '@relatos/content'
 
 export const NavBar = () => {
+  const navSections = Object.entries(hubConfig.sections)
   return (
     <nav className='bg-hub-background/0 fixed right-1/2 top-3 z-50 flex translate-x-1/2 gap-6 rounded-xl px-6 py-3 backdrop-blur-sm'>
-      {data.site.sections.map((section) => (
+      {navSections.map(([name, { title, slug, description }]) => (
         <Link
-          key={section.name}
-          href={section.url}
+          key={name}
+          href={slug}
           className='hover:text-hub-primary text-hub-background lowercase transition-colors'
+          aria-label={description}
         >
-          {section.name}
+          {title}
         </Link>
       ))}
     </nav>
--- apps/hub/src/components/home/ProjectNavigator.tsx
+++ apps/hub/src/components/home/ProjectNavigator.tsx
@@ -1,20 +1,25 @@
 import { BarrioCard } from '@/components/home/BarrioCard'
 import { NewBadge } from '@/components/NewBadge'
-import data from '@relatos/config/config.json'
+import { brandConfig, projectsConfig } from '@relatos/content'
+
+const { rengifo, covico } = projectsConfig
+
+const { rengifoUrl, covicoUrl } = {
+  rengifoUrl:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3002'
+      : brandConfig.paths.rengifo,
+  covicoUrl:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3001'
+      : brandConfig.paths.covico,
+}
 
 export const ProjectsNavigator = () => {
   return (
     <section className='mx-auto flex w-fit gap-20'>
-      <BarrioCard
-        href={'/'}
-        title={data.projects.rengifo.name}
-        imageSrc='/images/rengifo-image.png'
-      />
-      <BarrioCard
-        href={'/'}
-        title={data.projects.covico.name}
-        badge={<NewBadge />}
-      />
+      <BarrioCard href={rengifoUrl} title={rengifo.name} />
+      <BarrioCard href={covicoUrl} title={covico.name} badge={<NewBadge />} />
     </section>
   )
 }
