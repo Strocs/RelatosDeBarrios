81a0477557c745d105f17d2892957a67dea7c935
Strocs <strocsdev@gmail.com>
2025-07-21 16:59:19 -0400

    refactor: update Title component to use title prop and integrate Markdown for rendering

--- apps/rengifo-next/components/ui/Title.tsx
+++ apps/rengifo-next/components/ui/Title.tsx
@@ -1,25 +1,33 @@
 import { cn } from '@relatos/utils'
+import Markdown from 'react-markdown'
 
 interface TitleProps {
-  children: React.ReactNode
+  title: string
   className?: string
   headingLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 }
 
 export const Title = ({
-  children,
+  title,
   className,
   headingLevel = 'h1',
 }: TitleProps) => {
   const Heading = headingLevel
   return (
-    <Heading
-      className={cn(
-        'text-8xl font-black uppercase leading-none text-white',
-        className
-      )}
+    <Markdown
+      components={{
+        [Heading]: ({ ...props }) => (
+          <Heading
+            {...props}
+            className={cn(
+              'text-8xl font-bold uppercase leading-none text-white',
+              className
+            )}
+          />
+        ),
+      }}
     >
-      {children}
-    </Heading>
+      {title}
+    </Markdown>
   )
 }
