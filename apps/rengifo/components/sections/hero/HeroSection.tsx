611300cdcf17987cf48eec7876226a5a1def5036
Strocs <strocsdev@gmail.com>
2025-07-25 17:01:01 -0400

    refactor(rengifo): streamline content access in HeroSection
    This commit refactors the `HeroSection` component to streamline how content is accessed from the `heroContent` object. Instead of destructuring the properties, the component now accesses them directly, improving code readability and maintainability.

--- apps/rengifo/components/sections/hero/HeroSection.tsx
+++ apps/rengifo/components/sections/hero/HeroSection.tsx
@@ -1,29 +1,28 @@
-import { heroContent } from '@relatos/content/rengifo'
 import Image from 'next/image'
+import { heroContent } from '@relatos/content/rengifo'
 import { HeroTitleAction } from './HeroTitleAction'
 
 export const HeroSection = () => {
-  const { title, subTitle, utilityImages, actions, video } = heroContent
-
   return (
     <header id='inicio' className='relative h-screen landscape:h-[160dvh]'>
-      {/* Content Container with GSAP Parallax placeholder */}
-      <HeroTitleAction title={title} subTitle={subTitle} actions={actions} />
+      <HeroTitleAction
+        title={heroContent.title}
+        subTitle={heroContent.subTitle}
+        actions={heroContent.actions}
+      />
 
-      {/* Background Video */}
       <video
-        loop={video.loop}
-        muted={video.muted}
-        autoPlay={video.autoplay}
-        preload={video.preload}
+        loop={heroContent.video.loop}
+        muted={heroContent.video.muted}
+        autoPlay={heroContent.video.autoplay}
+        preload={heroContent.video.preload}
         className='pointer-events-none fixed inset-0 -z-10 aspect-video h-screen w-screen origin-center select-none object-cover'
       >
-        <source src={video.src} type='video/mp4' />
+        <source src={heroContent.video.src} type='video/mp4' />
       </video>
 
-      {/* Bottom Background Image */}
       <Image
-        {...utilityImages!.bg}
+        {...heroContent.utilityImages!.bg}
         className='pointer-events-none absolute -bottom-0 z-10 w-screen select-none'
       />
     </header>
