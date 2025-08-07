52926d82321061c65ffa9e400939797b65d2a949
Strocs <strocsdev@gmail.com>
2025-08-06 23:10:52 -0400

    refactor(layout): Add root layout and analytics

--- app/villacovico/(app)/layout.tsx
+++ app/villacovico/(app)/layout.tsx
@@ -23,12 +23,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      {children}
+    </body>
   )
 }
