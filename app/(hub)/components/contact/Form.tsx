2f34a3c1d79226b40d327633a7fe87acf709f151
Strocs <strocsdev@gmail.com>
2025-08-09 00:50:58 -0400

    refactor(contact): Update main form component to use server action
    This commit updates the main `Form` component to use the new `FormWithAction` component, simplifying the form structure and delegating the submission logic to the server action. The old `FormButton` component has been removed.

--- app/(hub)/components/contact/Form.tsx
+++ app/(hub)/components/contact/Form.tsx
@@ -1,28 +1,9 @@
-import { sendEmail } from '../../actions/contactAction'
 import { contactContent } from '../../content/contact'
-import { FormContribution } from './FormContribution'
-import { FormButton } from './FormButton'
-import { FormInput } from './FormInput'
+import { sendEmail } from '../../actions/contactAction'
+import { FormWithAction } from './FormWithAction'
 
-const { name, email, commentary, contribution, attachments, submit } =
-  contactContent.form
+const { form } = contactContent
 
 export const Form = () => {
-  return (
-    <form
-      action={sendEmail}
-      className='bg-hub-background/20 mx-auto w-full max-w-2xl rounded-2xl p-4 backdrop-blur-2xl md:p-8'
-    >
-      {/* Name field */}
-      <FormInput inputContent={name} />
-      {/* Email field */}
-      <FormInput inputContent={email} />
-      {/* Message/Commentary field */}
-      <FormInput inputContent={commentary} />
-      {/* Material contribution */}
-      <FormContribution contribution={contribution} attachments={attachments} />
-      {/* Submit button */}
-      <FormButton submitContent={submit} />
-    </form>
-  )
+  return <FormWithAction data={form} action={sendEmail} />
 }
