a159263c5288249397e0df22d55a669712e35bb0
Strocs <strocsdev@gmail.com>
2025-08-09 00:50:40 -0400

    refactor(contact): Update server action for email sending
    This commit refactors the `sendEmail` server action to handle file attachments correctly, use the new email template, and provide a proper response state.

--- app/(hub)/actions/contactAction.ts
+++ app/(hub)/actions/contactAction.ts
@@ -1,48 +1,70 @@
 'use server'
 import { brandContent } from '@/content/brand'
 import { contactContent } from '../content/contact'
-import { Attachment, CreateEmailOptions, Resend } from 'resend'
+import { Resend } from 'resend'
+import { EmailTemplate } from '../components/contact/EmailTemplate'
+import { ActionState, SendEmailResponse } from '../types/attachments'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const { name, email, commentary, contribution, attachments } =
-  contactContent.form
+const {
+  name: { id: name },
+  email: { id: email },
+  commentary: { id: commentary },
+  contribution: { id: contribution },
+  attachments: { id: attachments },
+} = contactContent.form
 
-export async function sendEmail(formData: FormData) {
-  const formName = formData.get(name.id) ?? ''
-  const formEmail = formData.get(email.id) ?? ''
-  const formCommentary = formData.get(commentary.id) ?? ''
-  const formContribution = formData.get(contribution.id) ?? ''
-  // TODO: have receive the attachments as an object with content, and filename
-  const formAttachments = formData.getAll(attachments.id) ?? []
+export async function sendEmail(
+  prevState: ActionState,
+  formData: FormData
+): SendEmailResponse {
+  const formName = (formData.get(name) ?? '') as string
+  const formEmail = (formData.get(email) ?? '') as string
+  const formCommentary = (formData.get(commentary) ?? '') as string
+  const formContribution = (formData.get(contribution) ?? '') as string
+  const formAttachments = (formData.getAll(attachments) ?? []) as File[]
 
   const projectContribution =
     brandContent.projects[
       formContribution as keyof typeof brandContent.projects
     ]
-  // server validations
 
-  const emailMessage: CreateEmailOptions = {
-    from: `${formName} <${formEmail}>`,
-    to: ['strocsdev@gmail.com'],
-    subject: 'Contacto desde el sitio web de ' + formName,
-    text: formCommentary as string,
-    attachments: formAttachments as Attachment[],
-    html: `
-       ${projectContribution ? `<p>Aporte: ${projectContribution}</p>` : ''} 
-        <p>Mensaje: ${formCommentary}</p>`,
-  }
+  const resendAttachments =
+    formAttachments.length > 0
+      ? await Promise.all(
+          formAttachments.map(async (file) => ({
+            content: Buffer.from(await file.arrayBuffer()).toString('base64'),
+            filename: file.name,
+          }))
+        )
+      : []
 
   // send email
   try {
-    const { data, error } = await resend.emails.send(emailMessage)
+    const { data, error } = await resend.emails.send({
+      from: `${formName} <web@contacto.relatosdebarrios.cl>`,
+      replyTo: formEmail,
+      to: ['strocsdev@gmail.com'],
+      subject: 'Contacto desde el sitio web de ' + formName,
+      text: formCommentary as string,
+      attachments: resendAttachments,
+      react: EmailTemplate({
+        name: formName,
+        commentary: formCommentary,
+        contribution: projectContribution,
+      }),
+    })
 
     if (error) {
       console.error('Error sending email:', error)
+      return { ok: false }
     }
 
     console.log('Email sent successfully:', data)
+    return { ok: true }
   } catch (error) {
     console.error(error)
+    return { ok: false }
   }
 }
